29d862b37a3be4aaba52211083aa6e13b035c261
Nick Hudson <nickhudson2244@gmail.com>
2020-05-09 14:58:57 -0400

    Added logout button styling

--- NDdb.php
+++ NDdb.php
@@ -76,6 +76,20 @@ float: left;
 		top: 0px;
 		position: absolute;
 	}
+	button {
+		background-color: #0015ff;
+		text-align: center;
+		font-size: 19px;
+		color: white;
+		border: 1px solid black;
+		height: 50px;
+		width: 150px;
+	}
+
+	button:hover{
+		 background-color: #85C1E9;
+	}
+
 
 	</style>
 </head>
